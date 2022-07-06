0ceb097221ac0ac35874cdb174a79039273dfbad
agostinopique <ago.pique@yahoo.it>
2022-07-06 09:06:11 +0200

    Modificato layout card;

--- resources/views/admin/posts/index.blade.php
+++ resources/views/admin/posts/index.blade.php
@@ -5,13 +5,14 @@
 
         @foreach($posts as $post)
 
-        <div class="card flex-shrink-0 m-3" style="width: 18rem;">
+        <div class="card flex-shrink-0 m-3 " style="width: 18rem;">
             <div class="card-body">
               <h5 class="card-title">{{ $post->title }}</h5>
               <h6 class="card-subtitle mb-2 text-muted">{{ $post->description }}</h6>
               <p class="card-text">{{ $post->content }}</p>
-              {{-- <a href="#" class="card-link">Card link</a> --}}
-              {{-- <a href="#" class="card-link">Another link</a> --}}
+              <a href="#" class="card-link">Show</a>
+              <a href="#" class="card-link">Edit</a>
+              <a href="#" class="card-link">Delete</a>
             </div>
           </div>
 
